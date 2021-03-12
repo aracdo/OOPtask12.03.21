e00e481f91cbe07d71b8bb1501e6c29b3cdde431
aracdo <40762424+aracdo@users.noreply.github.com>
2021-03-12 13:17:38 +0300

    Create task2.cpp

---
+++ task2.cpp
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+
+namespace OOPtest
+{
+    abstract class Book
+    {
+        private protected string author;
+        public Book(string author)
+        {
+            this.author = author;
+        }
+        public abstract Book getBook();
+        public abstract string getTitle();
+        public abstract void toString();
+    }
+
+    class MarkTwenBook : Book
+    {
+        private string title;
+        public MarkTwenBook(string title) : base("Mark Twen")
+        {
+            this.title = title;
+        }
+        public override void toString()
+        {
+            Console.WriteLine(title+" "+author );
+        }
+        public override string getTitle()
+        {
+            Console.WriteLine(title);
+            return title;
+        }
+        public override Book getBook()
+        {
+            return this;
+        }
+    }
+
+    class AgataCristiBook : Book
+    {
+        private string title;
+        public AgataCristiBook(string title) : base("Agata Cristi")
+        {
+            this.title = title;
+        }
+        public override void toString()
+        {
+            Console.WriteLine(title + " " + author);
+        }
+        public override string getTitle()
+        {
+            Console.WriteLine(title);
+            return title;
+        }
+        public override Book getBook()
+        {
+            return this;
+        }
+    }
+
+
+
+    
+
+        
+        class Program
+        {
+            static void Main(string[] args)
+            {
+            AgataCristiBook DeathOnNill = new AgataCristiBook("Death on the Nile read");
+            MarkTwenBook TomSawyer = new MarkTwenBook("The Adventures of Tom Sawyer");
+            DeathOnNill.toString();
+            TomSawyer.toString();
+            }
+        }
+}   
